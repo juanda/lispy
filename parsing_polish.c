76235d7ad0dbd6ca998cd64b00e6da4dc35b81dd
juanda rodriguez <juanda@juandarodriguez.es>
2018-10-18 07:12:47 +0000

    polish calculator complete

--- parsing_polish.c
+++ parsing_polish.c
@@ -1,24 +1,75 @@
 #include <stdio.h>
 #include "mpc/mpc.h"
 
-#include <readline/readline.h>
-#include <readline/history.h>
+#include "mpc/mpc.h"
+
+#ifdef __linux__
+#include <editline.h>
+#endif
+
+#ifdef __APPLE__
+#include <editline/readline.h>
+#include <editline/add_history.h>
+#endif
+
+
+int number_of_nodes(mpc_ast_t* t) {
+  if (t->children_num == 0) { return 1; }
+  if (t->children_num >= 1) {
+    int total = 1;
+    for (int i = 0; i < t->children_num; i++) {
+      total = total + number_of_nodes(t->children[i]);
+    }
+    return total;
+  }
+  return 0;
+}
+
+long eval_op(long x, char* op, long y){
+	if (strcmp(op, "+") == 0) { return x + y; }
+	if (strcmp(op, "-") == 0) { return x - y; }
+	if (strcmp(op, "*") == 0) { return x * y; }
+	if (strcmp(op, "/") == 0) { return x / y; }
+	return 0;
+}
+
+long eval(mpc_ast_t* t){
+	if(strstr(t->tag, "number")){
+		return atoi(t->contents);
+	}
+
+	char* op = t->children[1]->contents;
+
+	long x = eval(t->children[2]);
+
+    int i = 3;
+
+	while(strstr(t->children[i]->tag, "expr")){
+		x = eval_op(x, op, eval(t->children[i]));
+		i++;
+	}
+
+	return x;
+}
+
 
 int main(){
 
-  mpc_parser_t* Number   = mpc_new("number");
-  mpc_parser_t* Operator = mpc_new("operator");
-  mpc_parser_t* Expr     = mpc_new("expr");
-  mpc_parser_t* Lispy    = mpc_new("lispy");
+  mpc_parser_t* Dot       = mpc_new("dot");
+  mpc_parser_t* Number    = mpc_new("number");
+  mpc_parser_t* Operator  = mpc_new("operator");
+  mpc_parser_t* Expr      = mpc_new("expr");
+  mpc_parser_t* Lispy     = mpc_new("lispy");
 
   mpca_lang(MPCA_LANG_DEFAULT,
     "                                                     \
+      dot      : '.' ;                                    \
       number   : /-?[0-9]+/ ;                             \
-      operator : '+' | '-' | '*' | '/' ;                  \
+      operator : '+' | '-' | '*' | '/' | '%' ;            \
       expr     : <number> | '(' <operator> <expr>+ ')' ;  \
       lispy    : /^/ <operator> <expr>+ /$/ ;             \
     ",
-    Number, Operator, Expr, Lispy);
+    Dot, Number, Operator, Expr, Lispy);
   
   puts("Lispy version 0.1");
 
@@ -29,7 +80,10 @@ int main(){
     mpc_result_t r;
     if (mpc_parse("<stdin>", input, Lispy, &r)) {
       /* On Success Print the AST */
-      mpc_ast_print(r.output);
+      mpc_ast_t* a = r.output;
+	  long result = eval(a);
+      printf("%li\n", result);
+	  mpc_ast_print(a);
       mpc_ast_delete(r.output);
     } else {
       /* Otherwise Print the Error */
@@ -37,10 +91,8 @@ int main(){
       mpc_err_delete(r.error);
     }
 
-    printf("Yo have entered: %s\n", input);
-
     free(input);
   }
-  mpc_cleanup(4, Number, Operator, Expr, Lispy);
+  mpc_cleanup(4, Dot,Number, Operator, Expr, Lispy);
   return 0;
 }
